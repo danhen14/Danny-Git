3fde40d45eedba7ad58dd2aa6dcf95d8acb0e9fb
danhen14 <danhen14@yahoo.com>
2021-12-24 03:31:41 -0500

    Danny java loop

---
+++ eclipse-workspace/Danny-Java/src/craft/java/IfElse.java
@@ -0,0 +1,24 @@
+package craft.java;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		System.out.println("Silly Code");
+		System.out.println("I am going to fool you!");
+		
+		int time = 25;
+		if (time < 15) {
+			System.out.println("Good morning Danny!");
+		}	else if (time < 22) {
+			System.out.println("Have a great day Danny"); 
+		}	else {
+			System.out.println("Good evening Danny!");
+		}
+
+	}
+	
+}	
+	
+
+
+
