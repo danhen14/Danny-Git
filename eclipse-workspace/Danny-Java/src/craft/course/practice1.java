4f691103b2b3d1f7e48030adc3de6a8705fc9e02
danhen14 <danhen14@yahoo.com>
2021-12-23 23:07:23 -0500

    Eclipse commit

---
+++ eclipse-workspace/Danny-Java/src/craft/course/practice1.java
@@ -0,0 +1,12 @@
+package craft.course;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dan-dissel");
+		System.out.println("Danny");
+
+	}
+
+}
